03b4793cd484018850b6f5cd32e31e6cc35ff0b3
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-08-26 00:28:29 +0300

    Create orders.js

---
+++ Function/orders.js
@@ -0,0 +1,15 @@
+function orders(product, quantity) {
+    let price = 0;
+    switch (product) {
+        case "coffee": price = 1.50 * quantity; break;
+        case "water": price = 1.00 * quantity; break;
+        case "coke": price = 1.40 * quantity; break;
+        case "snack": price = 2.00 * quantity; break;
+
+    }
+
+    return price.toFixed(2)
+
+
+}
+console.log(orders("coffee", 2));
