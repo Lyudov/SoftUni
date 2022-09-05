026b8469d5a5e8240215322ad69cd74e2050dfae
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-09-05 22:50:49 +0300

    Create sumFirstAndLast.js

---
+++ Arrays Advance/sumFirstAndLast.js
@@ -0,0 +1,9 @@
+function sumFirstAndLast(arr) {
+
+    let firstElement = arr.shift()
+    let lastElement = arr.pop()
+    console.log(Number(firstElement) + Number(lastElement));
+
+}
+sumFirstAndLast(['20', '30', '40']);
+sumFirstAndLast(['5', '15']);
