3f7608a69314e361a7be1fb9a6b4078cc101656d
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-09-07 22:51:15 +0300

    Update myFilter.js

--- Arrays Advance/myFilter.js
+++ Arrays Advance/myFilter.js
@@ -4,5 +4,9 @@ function myFilter() {
     let longWords = myArr.filter(x => x.length > 3);
     console.log(longWords);
 
+    let nums = [5, -11, 3, -2, 8];
+    let positiveNums = nums.filter(x => x > 0);
+    console.log(positiveNums);
+
 }
 myFilter();
