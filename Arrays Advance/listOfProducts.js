1aabf61d90f3070ab98292ffcf0db5203d6f37aa
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-09-09 18:21:10 +0300

    Update listOfProducts.js

--- Arrays Advance/listOfProducts.js
+++ Arrays Advance/listOfProducts.js
@@ -1,5 +1,5 @@
 function listOfProducts(arr) {
-    let sortedArr = arr.sort((a, b) => a.localeCompare(b));
+    let sortedArr = arr.sort();
     for (let i = 0; i < sortedArr.length; i++) {
        
         console.log(`${i + 1}.${sortedArr[i]}`);
