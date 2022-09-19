970e426d91679c5c8e8cb2c9a4904d54e0ce3731
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-09-19 23:37:05 +0300

    Update login.js

--- Basic Syntax, Conditional Statements and Loops - Exercise/login.js
+++ Basic Syntax, Conditional Statements and Loops - Exercise/login.js
@@ -1,6 +1,30 @@
-function login() {
+function login(arr) {
 
-    let userName = input[0];
+    let userName = arr.shift();
+    let password = userName.split('').reverse().join('')
+    let trying = 0;
 
+    for (let i = 0; i <= arr.length - 1; i++) {
+        let command = arr[i]
+
+        while (command !== password) {
+            trying++;
+
+            if (trying > 3) {
+                console.log(`User ${userName} blocked!`);
+                break;
+            }
+
+            console.log(`Incorrect password. Try again.`);
+            command = arr.shift()
+            command = arr[i]
+        }
+
+        if (command === password) {
+            console.log(`User ${userName} logged in.`);
+        }
+    }
 }
-login();
+//login(['Acer', 'login', 'go', 'let me in', 'recA']);
+//login(['sunny', 'rainy', 'cloudy', 'sunny', 'not sunny']);
+login(['momo', 'omom']);
