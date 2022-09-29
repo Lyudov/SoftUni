bf634e3419668291de3153df0c15d2375917a64f
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-09-29 23:20:41 +0300

    Update reverseAnArrayOfNumbers.js

--- Arrays Lab/reverseAnArrayOfNumbers.js
+++ Arrays Lab/reverseAnArrayOfNumbers.js
@@ -1,9 +1,24 @@
-function reverseAnArrayOfNumbers(n, numbers) {
+function reverseAnArrayOfNumbers(num, array) {
 
-    let result = [];
-    for (let i = n - 1; i >= 0; i--) {
-        result.push(numbers[i]);
+    let newArray = '';
+    for (let i = 0; i < num; i++) {
+        newArray += array[num - 1 - i] + ' ';
     }
-    console.log(result.join(' '));
+
+    console.log(newArray);
+
 }
 reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]);
+reverseAnArrayOfNumbers(4, [-1, 20, 99, 5]);
+reverseAnArrayOfNumbers(2, [66, 43, 75, 89, 47]);
+
+
+// function reverseAnArrayOfNumbers(n, numbers) {
+
+//     let result = [];
+//     for (let i = n - 1; i >= 0; i--) {
+//         result.push(numbers[i]);
+//     }
+//     console.log(result.join(' '));
+// }
+// reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]);
