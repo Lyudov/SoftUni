2a47d4e142455998ff416a25c9ecc49929c31323
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-10-03 22:22:15 +0300

    Update addAndSubtract.js

--- Exercise Arrays/addAndSubtract.js
+++ Exercise Arrays/addAndSubtract.js
@@ -20,3 +20,31 @@ function addAndSubtract(arr) {
 
 }
 addAndSubtract([5, 15, 23, 56, 35]);
+
+
+// Another solution 
+
+// function addAndSubtract(array) {
+//     let sumArr = 0;
+//     let sumArrModified = 0;
+//     let modifiedArr = [];
+
+//     for (let i = 0; i < array.length; i++) {
+//         let current = array[i]
+
+//         sumArr += array[i]
+//         if (current % 2 === 0) {
+//             current += i;
+//         } else {
+//             current -= i
+//         }
+//         modifiedArr.push(current)
+//         sumArrModified += current;
+
+//     }
+//     console.log(modifiedArr);
+//     console.log(sumArr);
+//     console.log(sumArrModified);
+// }
+// //addAndSubtract([5, 15, 23, 56, 35]);
+// addAndSubtract([-5, 11, 3, 0, 2]);
