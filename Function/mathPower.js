93bd572fed11914d0f818c1ebf1e7424313ae9ac
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-10-06 22:50:20 +0300

    Update mathPower.js

--- Function/mathPower.js
+++ Function/mathPower.js
@@ -9,3 +9,12 @@ function mathPower(number, raised) {
     console.log(result);
 }
 mathPower(2, 8);
+
+
+// function mathPower(num, power) {
+
+//     return Math.pow(num, power);
+
+// }
+// let result = mathPower(2, 8)
+// console.log(result);
