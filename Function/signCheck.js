7e13d15a8f4c9450ada84b2a14edbdd57d77e8fb
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-10-06 23:26:48 +0300

    Update signCheck.js

--- Function/signCheck.js
+++ Function/signCheck.js
@@ -1,31 +1,49 @@
-function signCheck(num1, num2, num3) {
+function signCheck(numOne, numTwo, numThree) {
 
-    let res;
-    if (num1 < 0) {
-        if (num2 >= 0 && num3 >= 0) {
-            res = "Negative";
-        } else if (num2 < 0 && num3 < 0) {
-            res = "Negative";
-        } else if (num2 >= 0 && num3 < 0) {
-            res = "Positive";
-        } else if (num2 < 0 && num3 >= 0) {
-            res = "Positive";
-        }
-
-    } else if (num1 >= 0) {
-        if (num2 >= 0 && num3 >= 0) {
-            res = "Positive";
-        } else if (num2 < 0 && num3 < 0) {
-            res = "Positive";
-        } else if (num2 >= 0 && num3 < 0) {
-            res = "Negative";
-        } else if (num2 < 0 && num3 >= 0) {
-            res = "Negative";
-        }
-
+    let sum1 = numOne * numTwo;
+    let sum2 = numTwo * numThree;
+    let sum3 = numOne * numThree;
+    let totalSum = '';
+    if (sum1 >= 0 && numThree >= 0 || sum2 >= 0 && numOne >= 0 || sum3 >= 0 && numTwo >= 0) {
+        totalSum = "Positive";
+    } else {
+        totalSum = "Negative"
     }
-    console.log(res);
+    return totalSum
 }
-signCheck(-5,1,1);
-signCheck(5,14,-2);
-signCheck(19,-1,-5);
+console.log(signCheck(-6, -12, 14));
+console.log(signCheck(5, 12, -15));
+console.log(signCheck(-5, 1, 1)); 
+
+
+// function signCheck(num1, num2, num3) {
+
+//     let res;
+//     if (num1 < 0) {
+//         if (num2 >= 0 && num3 >= 0) {
+//             res = "Negative";
+//         } else if (num2 < 0 && num3 < 0) {
+//             res = "Negative";
+//         } else if (num2 >= 0 && num3 < 0) {
+//             res = "Positive";
+//         } else if (num2 < 0 && num3 >= 0) {
+//             res = "Positive";
+//         }
+
+//     } else if (num1 >= 0) {
+//         if (num2 >= 0 && num3 >= 0) {
+//             res = "Positive";
+//         } else if (num2 < 0 && num3 < 0) {
+//             res = "Positive";
+//         } else if (num2 >= 0 && num3 < 0) {
+//             res = "Negative";
+//         } else if (num2 < 0 && num3 >= 0) {
+//             res = "Negative";
+//         }
+
+//     }
+//     console.log(res);
+// }
+// signCheck(-5,1,1);
+// signCheck(5,14,-2);
+// signCheck(19,-1,-5);
