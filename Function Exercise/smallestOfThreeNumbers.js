7ec97e4bcc28f81bf66d53da2daf0f34760ae7ec
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-10-09 23:21:22 +0300

    Update smallestOfThreeNumbers.js

--- Function Exercise/smallestOfThreeNumbers.js
+++ Function Exercise/smallestOfThreeNumbers.js
@@ -5,3 +5,18 @@ function smallestOfThreeNumbers(first, second, last) {
 
 }
 smallestOfThreeNumbers(2,5,3);
+
+
+// function smallestOfThreeNumbers(numOne, numTwo, numThree) {
+
+//     let smallest = 0;
+//     if (numOne <= numTwo && numOne <= numThree) {
+//         smallest = numOne;
+//     } else if (numTwo <= numOne && numTwo <= numThree) {
+//         smallest = numTwo;
+//     } else if (numThree <= numOne && numThree <= numTwo) {
+//         smallest = numThree;
+//     }
+//     return smallest;
+// }
+// console.log(smallestOfThreeNumbers(2, 5, 3)); 
