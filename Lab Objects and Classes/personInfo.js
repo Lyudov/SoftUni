6b75f659231a4d0b41b935dc84f3a9930e450e41
Lyudov <104784031+Lyudov@users.noreply.github.com>
2022-11-26 23:55:38 +0200

    Create personInfo.js

---
+++ Lab Objects and Classes/personInfo.js
@@ -0,0 +1,10 @@
+function personInfo(firstName, lastName, age) {
+
+    let info = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    };
+    return info;
+}
+console.log(personInfo("George", "Smith", "18"));
