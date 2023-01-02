9a33a47c9230e112ce060e95b1b72721637f8288
Lyudov <104784031+Lyudov@users.noreply.github.com>
2023-01-02 18:53:16 +0200

    Create stringLength.js

---
+++ JS Advanced/Syntax, Functions and Statements/stringLength.js
@@ -0,0 +1,10 @@
+function stringLength(firstString, secondString, thirdString) {
+
+let sum = firstString.length + secondString.length + thirdString.length;
+let avg = sum / 3;
+console.log(sum);
+console.log(Math.floor(avg));
+
+}
+stringLength('chocolate', 'ice cream', 'cake');
+stringLength('pasta', '5', '22.3');
