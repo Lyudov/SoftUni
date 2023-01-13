863ae96fae325f45ff562bcfaf3c125178d40c5e
Lyudov <104784031+Lyudov@users.noreply.github.com>
2023-01-13 22:42:56 +0200

    Create evenPostionElement.js

---
+++ JS Advanced/Arrays and Nested Arrays/evenPostionElement.js
@@ -0,0 +1,13 @@
+function evenPostionElement(arr) {
+
+    let newArr = [];
+for(let i = 0; i <= arr.length; i++) {
+if(i % 2 === 0) {
+    newArr.push(arr[i]);
+}
+}
+console.log(newArr.join(' '));
+
+}
+evenPostionElement(['20', '30', '40','50', '60']);
+evenPostionElement(['10', '30']);
