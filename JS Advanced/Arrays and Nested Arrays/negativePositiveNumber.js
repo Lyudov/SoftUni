8997adab5305d1c86884ab3e03a26fb1420dd6ed
Lyudov <104784031+Lyudov@users.noreply.github.com>
2023-01-15 17:38:52 +0200

    Create negativePositiveNumber.js

---
+++ JS Advanced/Arrays and Nested Arrays/negativePositiveNumber.js
@@ -0,0 +1,13 @@
+function negativePositiveNumber(arr) {
+
+let newArr = [];
+for (const num of arr) {
+    if(num < 0 ) {
+        newArr.unshift(num)
+    } else {
+        newArr.push(num)
+    }
+}
+console.log(newArr.join('\n'));
+}
+negativePositiveNumber([7, -2, 8, 9]);
