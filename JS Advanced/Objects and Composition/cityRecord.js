502f26801d4d7876f26edbf868365ff57093dd50
Lyudov <104784031+Lyudov@users.noreply.github.com>
2023-01-22 22:57:36 +0200

    Create cityRecord.js

---
+++ JS Advanced/Objects and Composition/cityRecord.js
@@ -0,0 +1,14 @@
+function cityRecord(name, population, treasury) {
+
+    let infoCity = {
+        name: name,
+        population: population,
+        treasury: treasury,
+    }
+return infoCity;
+}
+cityRecord('Tortuga',
+
+    7000,
+
+    15000);
