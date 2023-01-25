959ae8bdca1aa5b5dcb29e351b7306dcb538b454
Lyudov <104784031+Lyudov@users.noreply.github.com>
2023-01-25 22:39:19 +0200

    Create app.js

---
+++ JS Advanced/DOM Introduction/Edit Element/app.js
@@ -0,0 +1,9 @@
+function editElement(ref, match, replacer) {
+       //const pattern = new RegExp(match,'g');
+       const text = ref.textContent;
+
+       //  const result = text.replace(pattern,replacer);
+       const result = text.split(match).join(replacer);
+
+         ref.textContent = result; 
+}
