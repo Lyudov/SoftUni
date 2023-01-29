40dd1ba83a8d3821c8058fe006d193ae679b7204
Lyudov <104784031+Lyudov@users.noreply.github.com>
2023-01-29 21:42:28 +0200

    Create app.js

---
+++ JS Advanced/DOM Introduction/Show More/app.js
@@ -0,0 +1,7 @@
+function showText() {
+   const read = document.getElementById('more');
+   const text = document.getElementById('text');
+   text.style.display = 'inline';
+   read.style.display = 'none';
+
+}
