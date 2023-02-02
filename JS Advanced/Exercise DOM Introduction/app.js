a59dbdf05b2796d24f575f1e8d67fcedddcb7ab1
Lyudov <104784031+Lyudov@users.noreply.github.com>
2023-02-02 23:08:09 +0200

    Delete JS Advanced/Exercise DOM Introduction directory

--- JS Advanced/Exercise DOM Introduction/app.js
+++
@@ -1,8 +0,0 @@
-function subtract() {
-    const num1 = document.getElementById("firstNumber").value
-    const num2 = document.getElementById("secondNumber").value
-    const result = document.getElementById("result");
-   
-    const sum = parseFloat(num1) - parseFloat(num2);
-    result.textContent = sum;
-}
